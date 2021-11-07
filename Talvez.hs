f8683a585bcc8b825602d0f000286209e809387c
Francisco Miamoto <fsmiamoto@gmail.com>
2021-11-07 02:31:36 -0300

    composition!

--- Talvez.hs
+++ Talvez.hs
@@ -8,8 +8,9 @@ firstThat :: (a -> Bool) -> [a] -> Maybe a
 firstThat _ []     = Nothing
 firstThat p (x:xs) =  if p x then Just x else firstThat p xs
 
+maybeize :: (a -> b) -> Maybe a -> Maybe b
+maybeize _ Nothing = Nothing
+maybeize f (Just a) = Just $ f a
+
 isGoodFirstThat :: (a -> Bool) -> (a -> Bool) -> [a] -> Maybe Bool
-isGoodFirstThat isgood p xs =
-    case firstThat p xs of
-        Just x -> Just $ isgood x
-        _ -> Nothing
+isGoodFirstThat isgood p = maybeize isgood . firstThat p
