c22a53b630460e73ae39252745ac4ac66d5082bf
Francisco Miamoto <francisco@dorayaki.co>
2021-11-12 23:24:58 -0300

    add moar problems

--- 99.hs
+++ 99.hs
@@ -24,3 +24,25 @@ elementAt :: [a] -> Int -> a
 elementAt [] _ = error "Empty list"
 elementAt (x:_) 1 = x
 elementAt (_:xs) index = elementAt xs (index-1)
+
+-- Problem 4
+myLength :: [a] -> Integer
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
+
+myLength' xs = myLengthIter xs 0
+    where 
+        myLengthIter [] n = n
+        myLengthIter (_:xs) n = myLengthIter xs (n+1)
+
+-- Problem 5
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+myReverse' [] = []
+myReverse' xs = last xs : myReverse' (init xs)
+
+-- Problem 6
+isPalindrome xs = xs == xs' where xs' = myReverse xs
+
